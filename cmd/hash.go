fb5f7b9617b268f8425a8e3b58145411caa6d5bd
Ryan Allen <rallen3882@gmail.com>
2019-07-06 00:54:55 -0400

    Moved hashing and encoding work into separate functions capable of handling multiple algorithms / encodings.  Also added new flags for these with reasonable defaults.

--- cmd/hash.go
+++ cmd/hash.go
@@ -10,11 +10,16 @@ func init() {
 	rootCmd.AddCommand(hashCmd)
 	hashCmd.Flags().StringVarP(&inFilePath, "in-file", "i", "", "File with strings to hash")
 	hashCmd.Flags().StringVarP(&outFilePath, "out-file", "o", "hashes.out", "File to write the strings and hashes")
+	hashCmd.Flags().StringVarP(&algorithm, "algorithm", "a", "", "Algorithm to use for hashing the strings")
+	hashCmd.Flags().StringVarP(&encoding, "encoding", "e", "hex", "Encoding to use for writing the hashed strings")
 	hashCmd.MarkFlagRequired("in-file")
+	hashCmd.MarkFlagRequired("algorithm")
 }
 
 var inFilePath string
 var outFilePath string
+var algorithm string
+var encoding string
 
 var hashCmd = &cobra.Command{
 	Use: "hash",
@@ -22,7 +27,7 @@ var hashCmd = &cobra.Command{
 	Short: "hash your strings",
 	Long: "hash strings from a file and writes the {string, hash} pairs to a file",
 	Run: func (cmd *cobra.Command, args []string) {
-		err := hash.FileMD5(inFilePath, outFilePath)
+		err := hash.FileHash(inFilePath, outFilePath, algorithm, encoding)
 		if err != nil {
 			log.Fatalln(err)
 		}
--- hash/md5.go
+++ hash/md5.go
@@ -4,16 +4,17 @@ import (
 	"bufio"
 	"crypto/md5"
 	"encoding/hex"
+	"errors"
 	"io"
 	"log"
 	"os"
 	"strings"
 )
 
-// FileMD5 accepts two string file paths, to an input and output.
+// FileHash accepts two string file paths, to an input and output.
 // The input file is read line by line, and a new file is written in the form
-// ^{string} : {md5hash}$
-func FileMD5(inFilePath string, outFilePath string) error {
+// ^{string} : {hash}$
+func FileHash(inFilePath string, outFilePath string, algorithm string, encoding string) error {
 	// Open the input file for reading, and the output file for writing
 	inFile, err := os.Open(inFilePath)
 	if err != nil {
@@ -40,22 +41,34 @@ func FileMD5(inFilePath string, outFilePath string) error {
 
 	// (Keep pumping 'til EOF)
 	for len(bytesRead) > 0 {
+
+		// Hash the bytes
+		bytesRead = bytesRead[:len(bytesRead)-1] // trim "\n" byte
+		hashedBytes, err := hashBytes(bytesRead, algorithm)
 		if err != nil {
 			return err
 		}
 
-		bytesRead = bytesRead[:len(bytesRead)-1] // trim "\n" byte
-		hashedBytes16 := md5.Sum(bytesRead)      // hash the bytes
-		hashedBytes := hashedBytes16[:]          // converts [16]byte to []byte
+		// encode them for readability
+		encodedHash, err := encode(hashedBytes, encoding)
+		if err != nil {
+			return err
+		}
 
 		// Write to the output buffer
-		stringBuilder.Write(bytesRead)
-		stringBuilder.WriteString(" : ")
-		stringBuilder.WriteString(hex.EncodeToString(hashedBytes))
-		stringBuilder.WriteString("\n")
+		stringBuilder, err = bufferHash(bytesRead, encodedHash, stringBuilder)
+		if err != nil {
+			return err
+		}
 
-		// Try reading the next line from the file
+		// Read the next line from the file
 		bytesRead, err = reader.ReadBytes('\n')
+		if err != nil {
+			if err.Error() == "EOF" {
+				break
+			}
+			return err
+		}
 	}
 
 	// flush the output buffer to file
@@ -63,6 +76,69 @@ func FileMD5(inFilePath string, outFilePath string) error {
 	if err != nil {
 		log.Fatalln(err)
 	}
-
 	return nil
+}
+
+// encode will apply an encoding format to data to make it more readable
+func encode(bytes []byte, encoding string) ([]byte, error) {
+
+	switch encoding {
+	case "hex":
+		encodedBytes := make([]byte, hex.EncodedLen(len(bytes)))
+		hex.Encode(encodedBytes, bytes)
+		return encodedBytes, nil
+	case "base64":
+		err := errors.New("base64 encoding is not implemented yet")
+		return nil, err
+	default:
+		err := errors.New("Invalid encoding format.  Supported formats are: hex, base64")
+		return nil, err
+	}
+
+}
+
+// hashBytes accepts a byte slice and a string describing the algorithm to use for hashing.
+// It will return a byte slice containing the hashed bytes, and an error if pertinent
+func hashBytes(bytes []byte, algorithm string) ([]byte, error) {
+
+	var hashedBytes []byte
+	// decide which hashing algorithm to use
+	switch algorithm {
+	case "md5":
+		hashedBytes16 := md5.Sum(bytes)
+		hashedBytes = hashedBytes16[:]
+	case "sha1":
+		err := errors.New("sha1 hashing is not implemented yet")
+		return nil, err
+	case "sha512":
+		err := errors.New("sha512 hashing is not implemented yet")
+		return nil, err
+	default:
+		err := errors.New("Invalid hash algorithm.  Supported algorithms are: md5, sha1, sha512")
+		return nil, err
+	}
+
+	return hashedBytes, nil
+}
+
+// bufferHash writes the rainbow table data to the output buffer in the proper format
+// It will return nothing but an error if pertinent
+func bufferHash(plainBytes []byte, hashedBytes []byte, buffer strings.Builder) (strings.Builder, error) {
+	_, err := buffer.Write(plainBytes)
+	if err != nil {
+		return buffer, err
+	}
+	_, err = buffer.WriteString(" : ")
+	if err != nil {
+		return buffer, err
+	}
+	_, err = buffer.Write(hashedBytes)
+	if err != nil {
+		return buffer, err
+	}
+	_, err = buffer.WriteString("\n")
+	if err != nil {
+		return buffer, err
+	}
+	return buffer, nil
 }
